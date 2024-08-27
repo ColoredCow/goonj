ce407d3de95c923257f5d4c5db2a1e38b8c4aa88
Tarun <tarun.joshi@coloredcow.com>
2024-08-27 18:55:57 +0530

    fix the class code

--- wp-content/civi-extensions/org.civicoop.civirules/CRM/GoonjCustom/Civirules/CollectionCampTrigger.php
+++ wp-content/civi-extensions/org.civicoop.civirules/CRM/GoonjCustom/Civirules/CollectionCampTrigger.php
@@ -1,39 +1,30 @@
 <?php
 
 class CRM_GoonjCustom_Civirules_CollectionCampTrigger extends CRM_Civirules_Trigger_Post {
-	/**
-	 * Returns an array of additional entities provided in this trigger
-	*
-	* @return array of CRM_Civirules_TriggerData_EntityDefinition
-	*/
 
-	protected function getAdditionalEntities() {
-		error_log("CollectionCampTrigger: getAdditionalEntities() called.");
+    protected function getAdditionalEntities() {
+        try {
+            error_log("CollectionCampTrigger: getAdditionalEntities() called.");
+            
+            $entities = parent::getAdditionalEntities();
+            error_log("CollectionCampTrigger: Entities - " . print_r($entities, TRUE));
 
-		// $entities = parent::getAdditionalEntities();
-		// error_log("CollectionCampTrigger: Entities - " . print_r($entities, TRUE));
+            return $entities;
+        } catch (\Exception $e) {
+            error_log("Error in getAdditionalEntities: " . $e->getMessage());
+        }
+    }
 
-		// $entities[] = new CRM_Civirules_TriggerData_EntityDefinition(
-		// 'Collection_Camp',
-		// 'Collection_Camp',
-		// 'CRM_Eck_DAO_CollectionCamp',
-		// 'Collection_Camp'
-		// );
-
-		return true;
-	}
-
-	/**
-	 * Alter the trigger data with extra data
-	 *
-	 * @param \CRM_Civirules_TriggerData_TriggerData $triggerData
-	 */
-
-	public function alterTriggerData(CRM_Civirules_TriggerData_TriggerData &$triggerData) {
-		error_log("CollectionCampTrigger: alterTriggerData() called.");
-		// $collectionCamp = $triggerData->getEntityData('Collection_Camp');
-		// error_log("CollectionCampTrigger: Collection Camp Data - " . print_r($collectionCamp, TRUE));
-		
-		// parent::alterTriggerData($triggerData);
-	}
+    public function alterTriggerData(CRM_Civirules_TriggerData_TriggerData &$triggerData) {
+        try {
+            error_log("CollectionCampTrigger: alterTriggerData() called.");
+            $collectionCamp = $triggerData->getEntityData('Collection_Camp');
+            error_log("CollectionCampTrigger: Collection Camp Data - " . print_r($collectionCamp, TRUE));
+        } catch (\Exception $e) {
+            error_log("Error in alterTriggerData: " . $e->getMessage());
+        }
+        
+        parent::alterTriggerData($triggerData);
+    }
 }
+
