4f00c9e235cbe39ba47de4c1b79d8072cafb589f
belwalshubham <belwalshubham08@gmail.com>
2024-10-09 10:39:33 +0530

    code cleanups

--- wp-content/themes/goonj-crm/functions.php
+++ wp-content/themes/goonj-crm/functions.php
@@ -270,9 +270,10 @@ function goonj_handle_user_identification_form() {
 			exit;
 		}
 
-		// If we are here, then it means for sure that the contact exists.
+		$allowedPurposes = ['material-contribution', 'dropping-center-contribution'];
 
-		if ( $purpose === 'material-contribution' || $purpose === 'dropping-center-contribution' ) {
+		// If we are here, then it means for sure that the contact exists.
+		if ( in_array( $purpose, $allowedPurposes ) ) {
 			$material_contribution_form_path = sprintf(
 				'/material-contribution/#?email=%s&phone=%s&Material_Contribution.Collection_Camp=%s&source_contact_id=%s',
 				$email,
