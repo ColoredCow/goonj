c29ea6c9bcf76ffd9252bc10179ad7c8f782cbc5
Tarun <tarun.joshi@coloredcow.com>
2024-10-10 14:34:39 +0530

    clanups

--- wp-content/civi-extensions/goonjcustom/Civi/CollectionCampVolunteerFeedbackService.php
+++ wp-content/civi-extensions/goonjcustom/Civi/CollectionCampVolunteerFeedbackService.php
@@ -78,7 +78,7 @@ class CollectionCampVolunteerFeedbackService extends AutoSubscriber {
       'from' => $from,
       'toEmail' => $volunteerEmailId,
       'replyTo' => $from,
-      'html' => self::getVolunteerFeedbackReminderEmailHtml($volunteerName, $collectionCampId, $volunteerEmailId),
+      'html' => self::getVolunteerFeedbackReminderEmailHtml($volunteerName, $collectionCampId),
     ];
 
     $emailSendResult = \CRM_Utils_Mail::send($mailParams);
@@ -97,18 +97,17 @@ class CollectionCampVolunteerFeedbackService extends AutoSubscriber {
    *
    * @param string $volunteerName
    * @param int $collectionCampId
-   * @param int $volunteerEmailId
    *
    * @return string
    */
-  public static function getVolunteerFeedbackReminderEmailHtml($volunteerName, $collectionCampId, $volunteerEmailId) {
+  public static function getVolunteerFeedbackReminderEmailHtml($volunteerName, $collectionCampId) {
     $homeUrl = \CRM_Utils_System::baseCMSURL();
     $feedbackFormUrl = $homeUrl . 'volunteer-camp-feedback/#?Eck_Collection_Camp1=' . $collectionCampId;
 
     $html = "
       <p>Dear $volunteerName,</p>
       <p>Greetings from Goonj!</p>
-      <p>Kindly reminding you to share your valuable feedback on the recent collection camp. Your insights are important for us to continue improving future in future camps.</p>
+      <p>Kindly reminding you to share your valuable feedback on the recent collection camp. Your insights are important for us to continue improving in future camps.</p>
       <p>If you haven’t had the chance yet, please take a few minutes to fill out the feedback form here:</p>
       <p><a href=\"$feedbackFormUrl\">Volunteer Feedback Form</a></p>
       <p>We look forward to working with you again!</p>
