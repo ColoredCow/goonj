2eb3b9df1166eac3b2d1da8bca0b2a7357a13e06
Nishant Kumar <nishant.kumar@colorecow.in>
2024-10-10 12:24:47 +0530

    resolve code rabbit feedbacks

--- wp-content/plugins/goonj-blocks/src/functions.php
+++ wp-content/plugins/goonj-blocks/src/functions.php
@@ -1,9 +1,18 @@
 <?php
 
-function gb_format_date($date) {
-    return $date->format('d-m-Y');
+function gb_format_date($date, $format = 'd-m-Y') {
+    if (!$date instanceof DateTime) {
+        throw new InvalidArgumentException('Input must be a DateTime object');
+    }
+    return $date->format($format);
 }
 
-function gb_format_time_range($start_date, $end_date) {
-    return $start_date->format('h:i A') . ' - ' . $end_date->format('h:i A');
+function gb_format_time_range($start_date, $end_date, $format = 'h:i A') {
+    if (!$start_date instanceof DateTime || !$end_date instanceof DateTime) {
+        throw new InvalidArgumentException('Both inputs must be DateTime objects');
+    }
+    if ($end_date <= $start_date) {
+        throw new InvalidArgumentException('End time must be after start time');
+    }
+    return $start_date->format($format) . ' - ' . $end_date->format($format);
 }
