c109ebb339274d882d6e9577d9b0f2d999aecee3
belwalshubham <belwalshubham08@gmail.com>
2024-10-15 14:34:39 +0530

    cleanups

--- wp-content/plugins/goonj-blocks/goonj-blocks.php
+++ wp-content/plugins/goonj-blocks/goonj-blocks.php
@@ -88,8 +88,6 @@ function gb_goonj_blocks_check_action_target_exists() {
 		'Collection_Camp_Intent_Details.Location_Area_of_camp',
 		'Collection_Camp_Intent_Details.City',
 		'Collection_Camp_Intent_Details.State',
-		'Dropping_Centre.Start_Time',
-		'Dropping_Centre.End_Time',
 		'Dropping_Centre.Where_do_you_wish_to_open_dropping_center_Address_',
 		'Dropping_Centre.State',
 		'Dropping_Centre.District_City',
