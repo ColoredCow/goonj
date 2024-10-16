4bd9a0e15a1c8577e43f9d141d03d17c197d72a5
belwalshubham <belwalshubham08@gmail.com>
2024-10-16 18:23:22 +0530

    code cleanups

--- wp-content/civi-extensions/goonjcustom/api/v3/Goonjcustom/DroppingCenterOutcomeCron.php
+++ wp-content/civi-extensions/goonjcustom/api/v3/Goonjcustom/DroppingCenterOutcomeCron.php
@@ -13,11 +13,11 @@ function civicrm_api3_goonjcustom_dropping_center_outcome_cron($params) {
   $droppingCenters = civicrm_api4('Eck_Collection_Camp', 'get', [
     'select' => [
       'Donation_Box_Register_Tracking.Cash_Contribution',
-      'Dropping_Centre.Donation_Tracking_Id',
+      'Dropping_Centre.Dropping_Center_Tracking_Id',
       'Donation_Box_Register_Tracking.Product_Sale_Amount_GBG_',
     ],
     'where' => [
-        ['Dropping_Centre.Donation_Tracking_Id', 'IS NOT NULL'],
+        ['Dropping_Centre.Dropping_Center_Tracking_Id', 'IS NOT NULL'],
     ],
     'checkPermissions' => FALSE,
   ]);
@@ -29,7 +29,7 @@ function civicrm_api3_goonjcustom_dropping_center_outcome_cron($params) {
   $bagsReceivedByTrackingId = [];
 
   foreach ($droppingCenters as $center) {
-    $trackingId = $center['Dropping_Centre.Donation_Tracking_Id'];
+    $trackingId = $center['Dropping_Centre.Dropping_Center_Tracking_Id'];
 
     if (!isset($cashContributionByTrackingId[$trackingId])) {
       $cashContributionByTrackingId[$trackingId] = 0;
@@ -39,52 +39,83 @@ function civicrm_api3_goonjcustom_dropping_center_outcome_cron($params) {
     }
 
     // Calculate cash contibution.
-    $cashContributionByTrackingId[$trackingId] += (int) ($center['Donation_Box_Register_Tracking.Cash_Contribution'] ?? 0);
+    $cashContributionByTrackingId[$trackingId] += (float) ($center['Donation_Box_Register_Tracking.Cash_Contribution'] ?? 0);
 
     // Calculate product sale amounts.
     $productSaleAmountByTrackingId[$trackingId] += (float) ($center['Donation_Box_Register_Tracking.Product_Sale_Amount_GBG_'] ?? 0);
 
-    // Calculate footfall.
-    $activities = civicrm_api4('Activity', 'get', [
-      'select' => ['id'],
-      'where' => [
-        ['activity_type_id:name', '=', 'Material Contribution'],
-        ['Material_Contribution.Dropping_Center', '=', $trackingId],
-      ],
-      'checkPermissions' => FALSE,
-    ]);
-    $footfallByTrackingId[$trackingId] = count($activities);
+  }
 
-    // Calculate vehicle dispatch count.
-    $collectionSourceVehicleDispatches = civicrm_api4('Eck_Collection_Source_Vehicle_Dispatch', 'get', [
-      'select' => ['Camp_Vehicle_Dispatch.Collection_Camp'],
-      'where' => [['Camp_Vehicle_Dispatch.Collection_Camp', '=', $trackingId]],
-      'checkPermissions' => FALSE,
-    ]);
-    $vehicleDispatchesByTrackingId[$trackingId] = count($collectionSourceVehicleDispatches);
+  // Calculate vehicle dispatch count.
+  $collectionSourceVehicleDispatches = civicrm_api4('Eck_Collection_Source_Vehicle_Dispatch', 'get', [
+    'select' => ['Camp_Vehicle_Dispatch.Collection_Camp'],
+    'where' => [['Camp_Vehicle_Dispatch.Collection_Camp', 'IS NOT NULL']],
+    'checkPermissions' => FALSE,
+  ]);
 
-    // Calculate number of bags received.
-    $bagData = civicrm_api4('Eck_Collection_Source_Vehicle_Dispatch', 'get', [
-      'select' => [
-        'Acknowledgement_For_Logistics.No_of_bags_received_at_PU_Office',
-        'Camp_Vehicle_Dispatch.Collection_Camp',
-      ],
-      'where' => [
-        ['Camp_Vehicle_Dispatch.Collection_Camp', '=', $trackingId],
-      ],
-      'checkPermissions' => FALSE,
-    ]);
+  $vehicleDispatchCount = [];
+  foreach ($collectionSourceVehicleDispatches as $dispatch) {
+    $dispatchId = $dispatch['Camp_Vehicle_Dispatch.Collection_Camp'];
 
-    $totalBags = 0;
-
-    foreach ($bagData as $record) {
-      if (!empty($record['Acknowledgement_For_Logistics.No_of_bags_received_at_PU_Office'])) {
-        $totalBags += (int) $record['Acknowledgement_For_Logistics.No_of_bags_received_at_PU_Office'];
-      }
+    if (!isset($vehicleDispatchCount[$dispatchId])) {
+      $vehicleDispatchCount[$dispatchId] = 0;
     }
+    $vehicleDispatchCount[$dispatchId] += 1;
+  }
 
-    // Store the totalBags count into $bagsReceivedByTrackingId for further use.
-    $bagsReceivedByTrackingId[$trackingId] = $totalBags;
+  foreach ($vehicleDispatchCount as $dispatchId => $count) {
+    $vehicleDispatchesByTrackingId[$dispatchId] = $count;
+  }
+
+  // Calculate the number of bags received.
+  $bagData = civicrm_api4('Eck_Collection_Source_Vehicle_Dispatch', 'get', [
+    'select' => [
+      'Acknowledgement_For_Logistics.No_of_bags_received_at_PU_Office',
+      'Camp_Vehicle_Dispatch.Collection_Camp',
+    ],
+    'where' => [
+      ['Camp_Vehicle_Dispatch.Collection_Camp', 'IS NOT NULL'],
+    ],
+    'checkPermissions' => FALSE,
+  ]);
+
+  $bagsReceivedByTrackingId = [];
+
+  foreach ($bagData as $record) {
+    $dispatchId = $record['Camp_Vehicle_Dispatch.Collection_Camp'];
+    // Default to 0 if empty.
+    $bagsReceived = (int) ($record['Acknowledgement_For_Logistics.No_of_bags_received_at_PU_Office'] ?? 0);
+
+    if (!isset($bagsReceivedByTrackingId[$dispatchId])) {
+      $bagsReceivedByTrackingId[$dispatchId] = 0;
+    }
+    $bagsReceivedByTrackingId[$dispatchId] += $bagsReceived;
+  }
+
+  // Calculate footfall.
+  $activities = civicrm_api4('Activity', 'get', [
+    'select' => ['id', 'Material_Contribution.Dropping_Center'],
+    'where' => [
+      ['activity_type_id:name', '=', 'Material Contribution'],
+      ['Material_Contribution.Dropping_Center', 'IS NOT NULL'],
+    ],
+    'checkPermissions' => FALSE,
+  ]);
+
+  $totalFootfall = [];
+
+  foreach ($activities as $activity) {
+    $droppingCenterId = $activity['Material_Contribution.Dropping_Center'];
+    if (!empty($droppingCenterId)) {
+      if (!isset($totalFootfall[$droppingCenterId])) {
+        $totalFootfall[$droppingCenterId] = 0;
+      }
+      $totalFootfall[$droppingCenterId] += 1;
+    }
+  }
+
+  foreach ($totalFootfall as $droppingCenterId => $footfallCount) {
+    $footfallByTrackingId[$droppingCenterId] = $footfallCount;
   }
 
   // Update Cash Contributions.
