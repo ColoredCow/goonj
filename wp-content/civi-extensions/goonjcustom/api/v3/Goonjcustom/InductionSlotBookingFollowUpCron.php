da76686464edbb47e8a949c85a39bd792a5bd19f
Nishant <nishantumu@gmail.com>
2024-10-30 14:51:37 +0530

    added check for 7 days

--- wp-content/civi-extensions/goonjcustom/api/v3/Goonjcustom/InductionSlotBookingFollowUpCron.php
+++ wp-content/civi-extensions/goonjcustom/api/v3/Goonjcustom/InductionSlotBookingFollowUpCron.php
@@ -4,27 +4,33 @@
  * @file
  */
 
-use Civi\Api4\EckEntity;
-use Civi\CollectionCampOutcomeService;
-use Civi\HelperService;
+use Civi\Api4\Activity;
+use Civi\Api4\MessageTemplate;
+
 
 /**
- * Goonjcustom.CollectionCampOutcomeReminderCron API specification (optional)
- * This is used for documentation and validation.
+ * Goonjcustom.induction_slot_booking_follow_up_cron API specification.
+ * 
+ * This function defines the API specification for the induction slot booking 
+ * follow-up cron job. It currently has no parameters.
  *
  * @param array $spec
- *   description of fields supported by this API call.
+ *   The specification for API fields supported by this cron job.
  *
  * @return void
  */
 function _civicrm_api3_goonjcustom_induction_slot_booking_follow_up_cron_spec(&$spec) {
-  // There are no parameters for the Goonjcustom cron.
+    // No parameters are needed for the Goonjcustom cron.
 }
 
 /**
- * Goonjcustom.CollectionCampOutcomeReminderCron API.
+ * Goonjcustom.induction_slot_booking_follow_up_cron API implementation.
+ *
+ * This function checks for unscheduled induction activities and sends follow-up
+ * emails to the respective contacts if they haven't already received one.
  *
  * @param array $params
+ *   Parameters passed to the API call.
  *
  * @return array API result descriptor
  *
@@ -34,28 +40,62 @@ function _civicrm_api3_goonjcustom_induction_slot_booking_follow_up_cron_spec(&$
  * @throws \CRM_Core_Exception
  */
 function civicrm_api3_goonjcustom_induction_slot_booking_follow_up_cron($params) {
-  $returnValues = [];
-  \Civi::log()->info('check');
-  $unscheduledInductionActivities = \Civi\Api4\Activity::get(FALSE)
-    ->addSelect('source_contact_id')
-    ->addWhere('activity_type_id:name', '=', 'Induction')
-    ->addWhere('status_id:name', '=', 'To be scheduled')
-    ->setLimit(25)
-    ->execute();
+	$returnValues = [];
+	\Civi::log()->info('Check for unscheduled induction activities');
 
-  foreach($unscheduledInductionActivities as $unscheduledInductionActivitie){
-    $template = \Civi\Api4\MessageTemplate::get(FALSE)
-      ->addWhere('msg_title', 'LIKE', 'Induction_slot_booking_follow_up_email%') // Adding '%' for right wildcard
-      ->setLimit(1)
-      ->execute()
-      ->single();
-    $emailParams = [
-      'contact_id' => $unscheduledInductionActivitie['source_contact_id'],
-      'template_id' => $template['id'],
-    ];
-    $emailResult = civicrm_api3('Email', 'send', $emailParams);
-    \Civi::log()->info('Email sent', ['result' => $emailResult]);
-  }
+	// Configurable number of days to check for scheduling
+	$followUpDays = 7;
+	$followUpTimestamp = strtotime("-$followUpDays days");
 
-  return civicrm_api3_create_success($returnValues, $params, 'Goonjcustom', 'induction_slot_booking_follow_up_cron');
+	try {
+			// Retrieve unscheduled induction activities
+			$unscheduledInductionActivities = Activity::get(FALSE)
+					->addSelect('source_contact_id', 'created_date')
+					->addWhere('activity_type_id:name', '=', 'Induction')
+					->addWhere('status_id:name', '=', 'To be scheduled')
+					->setLimit(25)
+					->execute();
+
+			foreach ($unscheduledInductionActivities as $activity) {
+					// Check if the created date is older than 7 days
+					if (strtotime($activity['created_date']) < $followUpTimestamp) {
+							// Fetch the email template for follow-up.
+							$template = MessageTemplate::get(FALSE)
+									->addSelect('id', 'msg_subject')
+									->addWhere('msg_title', 'LIKE', 'Induction_slot_booking_follow_up_email%')
+									->setLimit(1)
+									->execute()
+									->single();
+
+							// Check if an email has already been sent to avoid duplication.
+							$emailActivities = Activity::get(FALSE)
+									->addWhere('activity_type_id:name', '=', 'Email')
+									->addWhere('subject', '=', $template['msg_subject'])
+									->addWhere('source_contact_id', '=', $activity['source_contact_id'])
+									->setLimit(1)
+									->execute()->first();
+
+							if (!$emailActivities) {
+									// Prepare email parameters and send the email.
+									$emailParams = [
+											'contact_id' => $activity['source_contact_id'],
+											'template_id' => $template['id'],
+									];
+									$emailResult = civicrm_api3('Email', 'send', $emailParams);
+									\Civi::log()->info('Follow-up email sent', ['result' => $emailResult]);
+							} else {
+									\Civi::log()->info('Email already sent to contact', ['contact_id' => $activity['source_contact_id']]);
+							}
+					} else {
+							\Civi::log()->info('No follow-up needed, activity is recent', ['contact_id' => $activity['source_contact_id'], 'created_date'=>$activity['created_date']]);
+					}
+			}
+
+	} catch (Exception $e) {
+			// Log any errors encountered during the process.
+			\Civi::log()->error('Error in follow-up cron: ' . $e->getMessage());
+			return civicrm_api3_create_error($e->getMessage());
+	}
+
+	return civicrm_api3_create_success($returnValues, $params, 'Goonjcustom', 'induction_slot_booking_follow_up_cron');
 }
