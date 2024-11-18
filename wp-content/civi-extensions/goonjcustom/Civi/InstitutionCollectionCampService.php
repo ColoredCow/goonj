5927a8a93bbe4a5cc2ecb40049ea8eabf75aafe6
belwalshubham <belwalshubham08@gmail.com>
2024-11-18 12:25:32 +0530

    feat: Assign Goonj Office based on institution collection camp's state or fallback office

--- wp-content/civi-extensions/goonjcustom/Civi/InstitutionCollectionCampService.php
+++ wp-content/civi-extensions/goonjcustom/Civi/InstitutionCollectionCampService.php
@@ -4,20 +4,26 @@ namespace Civi;
 
 use Civi\Api4\CustomField;
 use Civi\Api4\StateProvince;
+use Civi\Traits\CollectionSource;
+use Civi\Api4\EckEntity;
+use Civi\Api4\Contact;
 use Civi\Core\Service\AutoSubscriber;
 
 /**
  *
  */
 class InstitutionCollectionCampService extends AutoSubscriber {
-
+    use CollectionSource;
+    const ENTITY_SUBTYPE_NAME = 'Institution_Collection_Camp';
+    const FALLBACK_OFFICE_NAME = 'Delhi';
   /**
    *
    */
   public static function getSubscribedEvents() {
     return [
       '&hook_civicrm_fieldOptions' => 'setIndianStateOptions',
-    ];
+      '&hook_civicrm_custom' => 'setOfficeDetails',
+    ];  
   }
 
   /**
@@ -57,4 +63,84 @@ class InstitutionCollectionCampService extends AutoSubscriber {
 
   }
 
+  private static function findStateField(array $array) {
+    $institutionCollectionCampStateField = CustomField::get(FALSE)
+      ->addSelect('id')
+      ->addWhere('name', '=', 'state')
+      ->addWhere('custom_group_id:name', '=', 'Institution_Collection_Camp_Intent')
+      ->execute()
+      ->first();
+
+    if (!$institutionCollectionCampStateField) {
+      return FALSE;
+    }
+
+    $stateFieldId = $institutionCollectionCampStateField['id'];
+
+    foreach ($array as $item) {
+      if ($item['entity_table'] === 'civicrm_eck_collection_camp' &&
+            $item['custom_field_id'] === $stateFieldId) {
+        return $item;
+      }
+    }
+
+    return FALSE;
+  }
+
+  private static function getFallbackOffice() {
+    $fallbackOffices = Contact::get(FALSE)
+      ->addSelect('id')
+      ->addWhere('organization_name', 'CONTAINS', self::FALLBACK_OFFICE_NAME)
+      ->execute();
+
+    return $fallbackOffices->first();
+  }
+
+
+  public static function setOfficeDetails($op, $groupID, $entityID, &$params) {
+    if ($op !== 'create' || self::getEntitySubtypeName($entityID) !== self::ENTITY_SUBTYPE_NAME) {
+      return;
+    }
+
+    if (!($stateField = self::findStateField($params))) {
+      return;
+    }
+
+    $stateId = $stateField['value'];
+    $institutionCollectionCampId = $stateField['entity_id'];
+
+    $institutionCollectionCampData = EckEntity::get('Collection_Camp', FALSE)
+      ->addSelect('Institution_Collection_Camp_Intent.Will_your_collection_drive_be_open_for_general_public_as_well')
+      ->addWhere('id', '=', $institutionCollectionCampId)
+      ->execute()->single();
+
+    $isPublicDriveOpen = $institutionCollectionCampData['Institution_Collection_Camp_Intent.Will_your_collection_drive_be_open_for_general_public_as_well'];
+
+    if (!$stateId) {
+      \CRM_Core_Error::debug_log_message('Cannot assign Goonj Office to institution collection camp: ' . $institutionCollectionCampData['id']);
+      return FALSE;
+    }
+
+    $officesFound = Contact::get(FALSE)
+      ->addSelect('id')
+      ->addWhere('contact_type', '=', 'Organization')
+      ->addWhere('contact_sub_type', 'CONTAINS', 'Goonj_Office')
+      ->addWhere('Goonj_Office_Details.Institution_Catchment', 'CONTAINS', $stateId)
+      ->execute();
+
+    $stateOffice = $officesFound->first();
+
+    // If no state office is found, assign the fallback state office.
+    if (!$stateOffice) {
+      $stateOffice = self::getFallbackOffice();
+    }
+
+    $stateOfficeId = $stateOffice['id'];
+
+    EckEntity::update('Collection_Camp', FALSE)
+      ->addValue('Institution_collection_camp_Review.Goonj_Office', $stateOfficeId)
+      ->addValue('Institution_Collection_Camp_Intent.Camp_Type', $isPublicDriveOpen)
+      ->addWhere('id', '=', $institutionCollectionCampId)
+      ->execute();
+  }
 }
