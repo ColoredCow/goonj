d34302e04b6beb7f76980995c7c625da44f0a065
belwalshubham <belwalshubham08@gmail.com>
2024-11-18 11:54:28 +0530

    feat: add Indian state options to Institution Collection Camp form

---
+++ wp-content/civi-extensions/goonjcustom/Civi/InstitutionCollectionCampService.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Civi;
+
+use Civi\Api4\CustomField;
+use Civi\Api4\StateProvince;
+use Civi\Core\Service\AutoSubscriber;
+
+/**
+ *
+ */
+class InstitutionCollectionCampService extends AutoSubscriber {
+
+  /**
+   *
+   */
+  public static function getSubscribedEvents() {
+    return [
+      '&hook_civicrm_fieldOptions' => 'setIndianStateOptions',
+    ];
+  }
+
+  /**
+   *
+   */
+  public static function setIndianStateOptions(string $entity, string $field, array &$options, array $params) {
+    if ($entity !== 'Eck_Collection_Camp') {
+      return;
+    }
+
+    $intentStateFields = CustomField::get(FALSE)
+      ->addWhere('custom_group_id:name', '=', 'Institution_Collection_Camp_Intent')
+      ->addWhere('name', '=', 'State')
+      ->execute();
+
+    $stateField = $intentStateFields->first();
+
+    $statefieldId = $stateField['id'];
+
+    if ($field !== "custom_$statefieldId") {
+      return;
+    }
+
+    $indianStates = StateProvince::get(FALSE)
+      ->addWhere('country_id.iso_code', '=', 'IN')
+      ->addOrderBy('name', 'ASC')
+      ->execute();
+
+    $stateOptions = [];
+    foreach ($indianStates as $state) {
+      if ($state['is_active']) {
+        $stateOptions[$state['id']] = $state['name'];
+      }
+    }
+
+    $options = $stateOptions;
+
+  }
+
+}
