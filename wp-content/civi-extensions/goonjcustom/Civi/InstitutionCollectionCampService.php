990731637964b3b7fd60e678aa52d770460b389e
belwalshubham <belwalshubham08@gmail.com>
2024-11-20 19:45:38 +0530

    code cleanups

--- wp-content/civi-extensions/goonjcustom/Civi/InstitutionCollectionCampService.php
+++ wp-content/civi-extensions/goonjcustom/Civi/InstitutionCollectionCampService.php
@@ -27,13 +27,13 @@ class InstitutionCollectionCampService extends AutoSubscriber {
   public static function getSubscribedEvents() {
     return [
       '&hook_civicrm_fieldOptions' => 'setIndianStateOptions',
-      '&hook_civicrm_pre' => 'generateDroppingCenterQr',
+      '&hook_civicrm_pre' => 'generateInstitutionCollectionCampQr',
       '&hook_civicrm_custom' => 'setOfficeDetails',
     ];
   }
 
 
-  public static function generateDroppingCenterQr(string $op, string $objectName, $objectId, &$objectRef) {
+  public static function generateInstitutionCollectionCampQr(string $op, string $objectName, $objectId, &$objectRef) {
     if ($objectName !== 'Eck_Collection_Camp' || !$objectId || !self::isCurrentSubtype($objectRef)) {
       return;
     }
@@ -54,11 +54,11 @@ class InstitutionCollectionCampService extends AutoSubscriber {
 
     // Check for status change.
     if ($currentStatus !== $newStatus && $newStatus === 'authorized') {
-      self::generateDroppingCenterQrCode($collectionCampId);
+      self::generateInstitutionCollectionCampQrCode($collectionCampId);
     }
   }
 
-  private static function generateDroppingCenterQrCode($id) {
+  private static function generateInstitutionCollectionCampQrCode($id) {
     $baseUrl = \CRM_Core_Config::singleton()->userFrameworkBaseURL;
     $data = "{$baseUrl}actions/insititution-collection-camp/{$id}";
 
