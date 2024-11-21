12074c632b8e096feb6ed333e76d875c539bc602
pokhiii <pokhi@coloredcow.com>
2024-11-21 15:05:43 +0530

    only one payment processor

--- wp-content/civi-extensions/civirazorpay/managed/PaymentProcessorType.mgd.php
+++ wp-content/civi-extensions/civirazorpay/managed/PaymentProcessorType.mgd.php
@@ -8,12 +8,13 @@ return [
             'version' => 3,
             'title' => 'Razorpay',
             'name' => 'Razorpay',
-            'description' => 'Razorpay Payment Processor for Contributions',
+            'description' => 'Razorpay Payment Processor for Contributions with Recurring support',
             'user_name_label' => 'API Key',
             'password_label' => 'Secret Key',
             'class_name' => 'Civirazorpay_Payment_Razorpay',
             'billing_mode' => 4, // Redirect
             'payment_type' => 1, // 1 = Credit Card
+            'is_recur' => TRUE,
         ],
     ],
 ];
