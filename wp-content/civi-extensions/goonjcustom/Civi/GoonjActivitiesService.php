74189c1e8e07ba6d397181cc1f31699b044c7a28
tarunnjoshi <tarun12joshi@gmail.com>
2024-12-05 11:54:32 +0530

    Fix the issue with urban ops on production

--- wp-content/civi-extensions/goonjcustom/Civi/GoonjActivitiesService.php
+++ wp-content/civi-extensions/goonjcustom/Civi/GoonjActivitiesService.php
@@ -354,35 +354,35 @@ class GoonjActivitiesService extends AutoSubscriber {
         'module' => 'afsearchCollectionCampActivity',
         'directive' => 'afsearch-collection-camp-activity',
         'template' => 'CRM/Goonjcustom/Tabs/CollectionCamp.tpl',
-        'permissions' => ['goonj_chapter_admin'],
+        'permissions' => ['goonj_chapter_admin', 'urbanops'],
       ],
       'logistics' => [
         'title' => ts('Logistics'),
         'module' => 'afsearchGoonjActivitiesLogistics',
         'directive' => 'afsearch-goonj-activities-logistics',
         'template' => 'CRM/Goonjcustom/Tabs/CollectionCamp.tpl',
-        'permissions' => ['goonj_chapter_admin'],
+        'permissions' => ['goonj_chapter_admin', 'urbanops'],
       ],
       'eventVolunteers' => [
         'title' => ts('Event Volunteers'),
         'module' => 'afsearchEventVolunteer',
         'directive' => 'afsearch-event-volunteer',
         'template' => 'CRM/Goonjcustom/Tabs/CollectionCamp.tpl',
-        'permissions' => ['goonj_chapter_admin'],
+        'permissions' => ['goonj_chapter_admin', 'urbanops'],
       ],
       'campOutcome' => [
         'title' => ts('Outcome'),
         'module' => 'afsearchGoonjActivitiesOutcomeView',
         'directive' => 'afsearch-goonj-activities-outcome-view',
         'template' => 'CRM/Goonjcustom/Tabs/CollectionCamp.tpl',
-        'permissions' => ['goonj_chapter_admin'],
+        'permissions' => ['goonj_chapter_admin', 'urbanops'],
       ],
       'campFeedback' => [
         'title' => ts('Volunteer Feedback'),
         'module' => 'afsearchVolunteerFeedback',
         'directive' => 'afsearch-volunteer-feedback',
         'template' => 'CRM/Goonjcustom/Tabs/CollectionCamp.tpl',
-        'permissions' => ['goonj_chapter_admin'],
+        'permissions' => ['goonj_chapter_admin', 'urbanops'],
       ],
       'monetaryContribution' => [
         'title' => ts('Monetary Contribution'),
@@ -396,7 +396,7 @@ class GoonjActivitiesService extends AutoSubscriber {
         'module' => 'afsearchMonetaryContributionForUrbanOps',
         'directive' => 'afsearch-monetary-contribution-for-urban-ops',
         'template' => 'CRM/Goonjcustom/Tabs/CollectionCamp.tpl',
-        'permissions' => ['goonj_chapter_admin'],
+        'permissions' => ['goonj_chapter_admin', 'urbanops'],
       ],
     ];
 
@@ -406,9 +406,16 @@ class GoonjActivitiesService extends AutoSubscriber {
         continue;
       }
 
-      $hasPermission = \CRM_Core_Permission::check($config['permissions']);
+      $hasPermission = false;
+      foreach ($config['permissions'] as $permission) {
+          if (\CRM_Core_Permission::check($permission)) {
+              $hasPermission = true;
+              break;
+          }
+      }
+      
       if (!$hasPermission) {
-        continue;
+          continue;
       }
 
       $tabs[$key] = [
