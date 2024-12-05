16434cab07e23bafe486351c01f03ad1de355de4
Nishant <nishantumu@gmail.com>
2024-12-05 19:04:59 +0530

    fix(*) induction slot days

--- wp-content/plugins/goonj-blocks/build/functions.php
+++ wp-content/plugins/goonj-blocks/build/functions.php
@@ -199,6 +199,13 @@ function generate_slots($assignedOfficeId, $maxSlots, $inductionType, $startDate
 function generateActivitySlots(&$slots, $maxSlots, $validInductionDays, $hour, $minute, $startDate, $scheduledActivityDates, &$slotCount, &$highActivityCountDays, $inductionType, $holidayDates) {
     $maxDays = 365;
     $holidayDatesArray = [];
+    \Civi::log()->info('startDate', ['startDate'=>$startDate]);
+    $currentDate = new DateTime();
+
+    // // Ensure start date is not in the past
+    if ($startDate < $currentDate) {
+        $startDate = clone $currentDate;
+    }
 
     if(!empty($holidayDates)){
         $holidayDatesArray = array_map('trim', explode(',', $holidayDates));
