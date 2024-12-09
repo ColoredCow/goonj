3a388c432333876d9778484263d43a7a06594f99
belwalshubham <belwalshubham08@gmail.com>
2024-12-09 18:02:31 +0530

    add landmark token

--- wp-content/civi-extensions/goonjcustom/CRM/Goonjcustom/Token/DroppingCenter.php
+++ wp-content/civi-extensions/goonjcustom/CRM/Goonjcustom/Token/DroppingCenter.php
@@ -27,6 +27,7 @@ class CRM_Goonjcustom_Token_DroppingCenter extends AbstractTokenSubscriber {
       'coordinator' => \CRM_Goonjcustom_ExtensionUtil::ts('Coordinator (Goonj)'),
       'remarks' => \CRM_Goonjcustom_ExtensionUtil::ts('Remarks'),
       'address_city' => \CRM_Goonjcustom_ExtensionUtil::ts('City'),
+      'landmark' => \CRM_Goonjcustom_ExtensionUtil::ts('Landmark or Nearby Area'),
     ]);
   }
 
@@ -80,6 +81,17 @@ class CRM_Goonjcustom_Token_DroppingCenter extends AbstractTokenSubscriber {
 
       case 'address_city':
         $value = $collectionSource['Dropping_Centre.District_City'];
+        break;
+
+      case 'landmark':
+        $landmarkOrNearbyArea = $collectionSource['Dropping_Centre.Landmark_or_Near_by_area'];
+
+        if (!empty($landmarkOrNearbyArea)) {
+          $value = "(" . $landmarkOrNearbyArea . ")";
+        }
+        else {
+          $value = '';
+        }
         break;
 
       default:
