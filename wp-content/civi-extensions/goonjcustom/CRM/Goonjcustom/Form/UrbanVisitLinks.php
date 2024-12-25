35fff85981935fd4e5b66d9e7238555dddda0ab5
tarunnjoshi <tarun12joshi@gmail.com>
2024-12-25 12:37:49 +0530

    Add visit share link: initial setuo

---
+++ wp-content/civi-extensions/goonjcustom/CRM/Goonjcustom/Form/UrbanVisitLinks.php
@@ -0,0 +1,182 @@
+<?php
+
+/**
+ * @file
+ * Form controller class.
+ *
+ * @see https://docs.civicrm.org/dev/en/latest/framework/quickform/
+ */
+
+/**
+ *
+ */
+class CRM_Goonjcustom_Form_UrbanVisitLinks extends CRM_Core_Form {
+
+  /**
+   * Urban Visit Id.
+   *
+   * @var int
+   */
+  public $_urbanVisitId;
+
+  /**
+   * Goonj coordinator Goonj POC contact Id.(Outcome)
+   *
+   * @var int
+   */
+  public $_contactId;
+
+  /**
+   * Goonj External Coordinator POC id.(Feedback)
+   *
+   * @var int
+   */
+  public $_processingCenterId;
+
+  /**
+   * Preprocess .
+   */
+  public function preProcess() {
+    $this->_urbanVisitId = CRM_Utils_Request::retrieve('ccid', 'Positive', $this);
+
+    $this->setTitle('Urban Visit Links');
+    parent::preProcess();
+  }
+
+  /**
+   * Set default values for the form.
+   *
+   * For edit/view mode the default values are retrieved from the database.
+   *
+   * @return array
+   */
+  public function setDefaultValues() {
+    $defaults = [];
+
+    if (!empty($this->_contactId)) {
+      $defaults['contact_id'] = $this->_contactId;
+      $this->generateLinks($this->_contactId);
+    }
+
+    return $defaults;
+  }
+
+  /**
+   * Function to generate urban visit related links.
+   *
+   * @param int $contactId
+   *   Contact Id.
+   *
+   * @return void
+   */
+  public function generateLinks($contactId): void {
+
+    // Generate urban visit links.
+    $links = [
+      [
+        'label' => 'Visit Outcome',
+        'url' => self::createUrl(
+                '/visit-outcome',
+                "Eck_Institution_Visit1={$this->_urbanVisitId}",
+        ),
+      ],
+      [
+        'label' => 'Visit Feedback',
+        'url' => self::createUrl(
+                '/visit-feedback',
+                "Eck_Institution_Visit1={$this->_urbanVisitId}",
+        ),
+      ],
+    ];
+
+    $this->assign('UrbanVisitLinks', $links);
+  }
+
+  /**
+   * Generate an authenticated URL for viewing this form.
+   *
+   * @param string $path
+   * @param string $params
+   * @param int $contactId
+   *
+   * @return string
+   *
+   * @throws \Civi\Crypto\Exception\CryptoException
+   */
+  public static function createUrl($path, $params, $contactId): string {
+    $expires = \CRM_Utils_Time::time() +
+      (\Civi::settings()->get('checksum_timeout') * 24 * 60 * 60);
+
+    /** @var \Civi\Crypto\CryptoJwt $jwt */
+    $jwt = \Civi::service('crypto.jwt');
+
+    $bearerToken = "Bearer " . $jwt->encode([
+      'exp' => $expires,
+      'sub' => "cid:" . $contactId,
+      'scope' => 'authx',
+    ]);
+
+    $params = "{$params}&_authx={$bearerToken}&_authxSes=1";
+    // $url = \CRM_Utils_System::url($path, $params, TRUE, NULL, FALSE, TRUE);
+    $config = CRM_Core_Config::singleton();
+    $url = $config->userFrameworkBaseURL . $path . '#?' . $params;
+
+    return $url;
+  }
+
+  /**
+   * @throws \CRM_Core_Exception
+   */
+  public function buildQuickForm(): void {
+
+    // Add form elements.
+    $this->addEntityRef('contact_id', ts('Contact to send'), [], TRUE);
+
+    $this->addButtons([
+    [
+      'type' => 'submit',
+      'name' => \CRM_Goonjcustom_ExtensionUtil::ts('Generate Links'),
+      'isDefault' => TRUE,
+    ],
+    ]);
+
+    // Export form elements.
+    $this->assign('elementNames', $this->getRenderableElementNames());
+    parent::buildQuickForm();
+  }
+
+  /**
+   *
+   */
+  public function postProcess(): void {
+    $values = $this->exportValues();
+
+    if (!empty($values['contact_id'])) {
+      $this->generateLinks($values['contact_id']);
+    }
+
+    parent::postProcess();
+  }
+
+  /**
+   * Get the fields/elements defined in this form.
+   *
+   * @return array (string)
+   */
+  public function getRenderableElementNames(): array {
+    // The _elements list includes some items which should not be
+    // auto-rendered in the loop -- such as "qfKey" and "buttons".  These
+    // items don't have labels.  We'll identify renderable by filtering on
+    // the 'label'.
+    $elementNames = [];
+    foreach ($this->_elements as $element) {
+      /** @var HTML_QuickForm_Element $element */
+      $label = $element->getLabel();
+      if (!empty($label)) {
+        $elementNames[] = $element->getName();
+      }
+    }
+    return $elementNames;
+  }
+
+}
