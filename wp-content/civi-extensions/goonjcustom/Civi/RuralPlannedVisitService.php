c2e832a7006fce1752bbaae847a725fd76703569
Nishant <nishantumu@gmail.com>
2025-01-06 13:18:19 +0530

    added cron to send rural planned visit outcome email

--- wp-content/civi-extensions/goonjcustom/Civi/RuralPlannedVisitService.php
+++ wp-content/civi-extensions/goonjcustom/Civi/RuralPlannedVisitService.php
@@ -4,6 +4,8 @@ namespace Civi;
 
 use Civi\Core\Service\AutoSubscriber;
 use Civi\Api4\Event;
+use Civi\Api4\Address;
+use Civi\Api4\Contact;
 
 /**
  *
@@ -134,6 +136,89 @@ class RuralPlannedVisitService extends AutoSubscriber {
       \Civi::service('angularjs.loader')->addModules($config['module']);
     }
 
+  }
+
+  /**
+   *
+   */
+  public static function sendRuralPlannedVisitOutcomeEmail($event) {
+    try {
+      $eventId = $event['id'];
+      $eventCode = $event['title'];
+      $addresses = Address::get(FALSE)
+        ->addWhere('id', '=', $event['loc_block_id.address_id'])
+        ->setLimit(1)
+        ->execute()->first();
+      $eventAddress = \CRM_Utils_Address::format($addresses);
+
+      $eventAttendedById = $event['Rural_Planned_Visit.Goonj_Coordinator'];
+      $outcomeEmailSent = $event['Rural_Planned_Visit_Outcome.Outcome_Email_Sent'];
+
+      $startDate = new \DateTime($event['start_date']);
+
+      $today = new \DateTimeImmutable();
+      $endOfToday = $today->setTime(23, 59, 59);
+
+      if (!$outcomeEmailSent && $startDate <= $endOfToday) {
+        $eventAttendedBy = Contact::get(FALSE)
+          ->addSelect('email.email', 'display_name')
+          ->addJoin('Email AS email', 'LEFT')
+          ->addWhere('id', '=', $eventAttendedById)
+          ->execute()->first();
+
+        $attendeeEmail = $eventAttendedBy['email.email'];
+        $attendeeName = $eventAttendedBy['display_name'];
+        $from = HelperService::getDefaultFromEmail();
+
+        if (!$attendeeEmail) {
+          throw new \Exception('Attendee email missing');
+        }
+
+        $mailParams = [
+          'subject' => 'Rural Planned Visit Events Notification: ' . $eventCode . ' at ' . $eventAddress,
+          'from' => $from,
+          'toEmail' => $attendeeEmail,
+          'replyTo' => $from,
+          'html' => self::getOutcomeEmailHtml($attendeeName, $eventId, $eventAttendedById, $eventCode, $eventAddress),
+        ];
+
+        $emailSendResult = \CRM_Utils_Mail::send($mailParams);
+
+        if ($emailSendResult) {
+          Event::update(FALSE)
+            ->addValue('Rural_Planned_Visit_Outcome.Outcome_Email_Sent', TRUE)
+            ->addValue('Rural_Planned_Visit_Outcome.Filled_By', $eventAttendedById)
+            ->addWhere('id', '=', $eventId)
+            ->execute();
+        }
+      }
+    }
+    catch (\Exception $e) {
+      \Civi::log()->error("Error in sendEventsOutcomeEmail for $eventId " . $e->getMessage());
+    }
+
+  }
+
+  /**
+   *
+   */
+  private static function getOutcomeEmailHtml($attendeeName, $eventId, $eventAttendedById, $eventCode, $eventAddress) {
+    $homeUrl = \CRM_Utils_System::baseCMSURL();
+    // Construct the full URLs for the forms.
+    $eventOutcomeFormUrl = $homeUrl . 'rural-planned-visit-outcome/#?Event1=' . $eventId;
+
+    $html = "
+	<p>Dear $attendeeName,</p>
+	<p>Thank you for attending the Rural Planned Visit <strong>$eventCode</strong> at <strong>$eventAddress</strong>. Their is one forms that require your attention during and after the Rural Planned Visit:</p>
+	<ol>
+		Please complete this form from the Rural Planned Visit location once the event ends.</li>
+		<li><a href=\"$eventOutcomeFormUrl\">Rural Planned Visit Outcome Form</a><br>
+		This feedback form should be filled out after the event ends, once you have an overview of the event's outcomes.</li>
+	</ol>
+	<p>We appreciate your cooperation.</p>
+	<p>Warm Regards,<br>Urban Relations Team</p>";
+
+    return $html;
   }
 
 }
---
+++ wp-content/civi-extensions/goonjcustom/api/v3/Goonjcustom/RuralPlannedVisitOutcomeCron.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @file
+ */
+
+use Civi\Api4\Event;
+use Civi\RuralPlannedVisitService;
+
+/**
+ * Goonjcustom.RuralPlannedVisitOutcomeCron API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec
+ *   description of fields supported by this API call.
+ *
+ * @return void
+ */
+function _civicrm_api3_goonjcustom_rural_planned_visit_outcome_cron_spec(&$spec) {
+  // There are no parameters for the Goonjcustom cron.
+}
+
+/**
+ * Goonjcustom.RuralPlannedVisitOutcomeCron API.
+ *
+ * @param array $params
+ *
+ * @return array API result descriptor
+ *
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ *
+ * @throws \CRM_Core_Exception
+ */
+function civicrm_api3_goonjcustom_rural_planned_visit_outcome_cron($params) {
+  $returnValues = [];
+
+  $today = new DateTimeImmutable();
+  $endOfDay = $today->setTime(23, 59, 59)->format('Y-m-d H:i:s');
+  $events = Event::get(TRUE)
+    ->addSelect('title', 'loc_block_id.address_id', 'Rural_Planned_Visit.Goonj_Coordinator', 'Rural_Planned_Visit.External_Coordinating_Poc', 'start_date', 'end_date', 'Rural_Planned_Visit_Outcome.Outcome_Email_Sent')
+    ->addWhere('event_type_id:name', '=', 'Rural Planned Visit')
+    ->addWhere('Rural_Planned_Visit.Status:name', '=', 'Authorized')
+    ->addWhere('start_date', '<=', $endOfDay)
+    ->addClause('OR', ['Rural_Planned_Visit_Outcome.Outcome_Email_Sent', 'IS NULL'], ['Rural_Planned_Visit_Outcome.Outcome_Email_Sent', '=', FALSE])
+    ->setLimit(25)
+    ->execute();
+  \Civi::log()->info('events', ['events' => $events]);
+
+  foreach ($events as $event) {
+
+    try {
+      RuralPlannedVisitService::sendRuralPlannedVisitOutcomeEmail($event);
+    }
+    catch (\Exception $e) {
+      \Civi::log()->info('Error Rural Planned Visit Outcome Cron', [
+        'id' => $event['id'],
+        'error' => $e->getMessage(),
+      ]);
+    }
+  }
+
+  return civicrm_api3_create_success($returnValues, $params, 'Goonjcustom', 'rural_planned_visit_outcome_cron');
+}
