f2221558faf6cba7d445052e3ed761f500cdcfd7
deepankar-sajwan <deepankar.sajwan@coloredcow.in>
2025-02-26 00:05:14 +0530

    added code for regitsration

--- playwright/e2e/collection-camp-registration.spec.js
+++ playwright/e2e/collection-camp-registration.spec.js
@@ -1,25 +1,28 @@
 import { test, expect } from '@playwright/test';
-import { userDetails, userLogin, submitVolunteerRegistrationForm, searchAndVerifyContact, userLogout, submitCollectionCampRegistrationForm, collectionCampUserDetails } from '../utils.js';
+import { userDetails, userLogin, submitVolunteerRegistrationForm, searchAndVerifyContact, userLogout, submitCollectionCampRegistrationForm, collectionCampUserDetails, urbanOpsUserLogin} from '../utils.js';
 import { VolunteerProfilePage } from '../pages/volunteer-profile.page';
 import { InductedVolunteerPage } from '../pages/inducted-volunteer.page';
 import { CollectionCampPage } from '../pages/collection-camp-registration.page'; // Adjust the path
+import { CollectionCampRecordsPage  } from '../pages/collection-camp-records.page'; // Adjust the path
 
 
 test.describe('Collection camp registration', () => {
   let volunteerProfilePage;
   let inductedVolunteerPage
   let collectionCampPage
+  let collectionCampRecordsPage
   const contactType = 'Individual';
   test.beforeEach(async ({ page }) => {
     volunteerProfilePage = new VolunteerProfilePage(page);
     inductedVolunteerPage  = new InductedVolunteerPage(page);
     collectionCampPage = new CollectionCampPage(page);
+    collectionCampRecordsPage =  new CollectionCampRecordsPage(page);
     await submitVolunteerRegistrationForm(page, userDetails);
     await page.waitForTimeout(2000);
     await userLogin(page);
   });
 
-  test('schedule induction and update induction status as completed', async ({ page }) => {
+  test('Verifying collection camp is created after form submission', async ({ page }) => {
     let userEmailAddress = userDetails.email.toLowerCase()
     let userMobileNumber = userDetails.mobileNumber
     const updatedStatus = 'Completed'
@@ -37,7 +40,11 @@ test.describe('Collection camp registration', () => {
     await page.waitForTimeout(3000)
     await userLogout(page)
     await submitCollectionCampRegistrationForm(page, userEmailAddress, userMobileNumber, collectionCampUserDetails)
-
+    await page.waitForTimeout(3000)
+    await urbanOpsUserLogin(page)
+    await page.waitForTimeout(3000)
+    await collectionCampRecordsPage.clickReviewLinCollectionCamp(userDetails.firstName, collectionCampUserDetails.state)
+    await collectionCampRecordsPage.selectCampaignForCollectionCamp('Rahat')
 });
     
-  });
+});
