d349c42d8eeb75f462a2737663a2eec9abe83ca0
Nishant <nishantumu@gmail.com>
2025-03-10 11:35:52 +0530

    fix(*) check group chapter assigned

--- wp-content/civi-extensions/goonjcustom/Civi/UrbanPlannedVisitService.php
+++ wp-content/civi-extensions/goonjcustom/Civi/UrbanPlannedVisitService.php
@@ -575,6 +575,15 @@ class UrbanPlannedVisitService extends AutoSubscriber {
     }
 
     $groupId = self::getChapterGroupForState($stateProvinceId);
+    // Check if already assigned to group chapter
+		$groupContacts = GroupContact::get(FALSE)
+			->addWhere('contact_id', '=', $contact_id)
+			->addWhere('group_id', '=', $groupId)
+			->execute()->first();
+
+		if (!empty($groupContacts)) {
+			return;
+		}
 
     if ($groupId & $contactId) {
       GroupContact::create(FALSE)
