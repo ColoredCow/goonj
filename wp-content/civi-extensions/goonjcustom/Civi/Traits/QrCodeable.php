83e838756e4b756cc5cdd7a11ce6fe2fdb108f12
tarunnjoshi <tarun12joshi@gmail.com>
2025-04-10 11:51:28 +0530

    Generate receipt for contribution

--- wp-content/civi-extensions/goonjcustom/Civi/Traits/QrCodeable.php
+++ wp-content/civi-extensions/goonjcustom/Civi/Traits/QrCodeable.php
@@ -5,6 +5,7 @@ namespace Civi\Traits;
 use chillerlan\QRCode\QRCode;
 use chillerlan\QRCode\QROptions;
 use Civi\Api4\CustomField;
+use Dompdf\Dompdf;
 
 /**
  *
@@ -101,6 +102,97 @@ trait QrCodeable {
 
     return $attachment;
 
+  }
+
+  /**
+   *
+   */
+  public static function generatePdfForCollectionCamp($entityId) {
+    try {
+      $dompdf = new Dompdf(['isRemoteEnabled' => TRUE]);
+
+      $html = '
+        <h1>Collection Camp Acknowledgement</h1>
+        <p>This is to acknowledge the material contribution.</p>
+        <p>Activity ID: ' . $entityId . '</p>
+      ';
+
+      $dompdf->loadHtml($html);
+      $dompdf->setPaper('A4', 'portrait');
+      $dompdf->render();
+
+      $pdfOutput = $dompdf->output();
+      $fileName = "acknowledgement_{$entityId}.pdf";
+      $tempFilePath = \CRM_Utils_File::tempnam($fileName);
+
+      file_put_contents($tempFilePath, $pdfOutput);
+
+      // Save to custom field.
+      return self::savePdfAttachmentToCustomField(
+        $entityId,
+        $fileName,
+        $tempFilePath,
+      // Your custom group name.
+        'Material_Contribution',
+      // Your custom field name.
+        'Receipt'
+      );
+
+    }
+    catch (\Exception $e) {
+      \CRM_Core_Error::debug_log_message('PDF error: ' . $e->getMessage());
+      return FALSE;
+    }
+  }
+
+  /**
+   *
+   */
+  public static function savePdfAttachmentToCustomField($entityId, $fileName, $filePath, $customGroupName, $customFieldName) {
+    error_log("entityId: " . print_r($entityId, TRUE));
+    error_log("fileName: " . print_r($fileName, TRUE));
+    error_log("filePath: " . print_r($filePath, TRUE));
+    error_log("customGroupName: " . print_r($customGroupName, TRUE));
+    error_log("customFieldName: " . print_r($customFieldName, TRUE));
+
+    $customFields = CustomField::get(FALSE)
+      ->addSelect('id')
+      ->addWhere('custom_group_id:name', '=', $customGroupName)
+      ->addWhere('name', '=', $customFieldName)
+      ->setLimit(1)
+      ->execute();
+
+    $field = $customFields->first();
+    error_log("field: " . print_r($field, TRUE));
+
+    if (!$field) {
+      \CRM_Core_Error::debug_log_message("Custom field not found: {$customGroupName} / {$customFieldName}");
+      return FALSE;
+    }
+
+    $fieldId = 'custom_' . $field['id'];
+    error_log("fieldId: " . print_r($fieldId, TRUE));
+
+    $params = [
+      'entity_id' => $entityId,
+      'name' => $fileName,
+      'mime_type' => 'application/pdf',
+      'field_name' => $fieldId,
+      'options' => [
+        'move-file' => $filePath,
+      ],
+    ];
+    error_log("params: " . print_r($params, TRUE));
+
+    $result = civicrm_api3('Attachment', 'create', $params);
+    error_log("result: " . print_r($result, TRUE));
+
+    if (!empty($result['is_error'])) {
+      \CRM_Core_Error::debug_log_message("Failed to attach PDF for entity ID $entityId");
+      return FALSE;
+    }
+
+    return $result['values'][$result['id']];
   }
 
 }
