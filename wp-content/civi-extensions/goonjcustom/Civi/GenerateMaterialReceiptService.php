2735aa38d7c0bc4f9255cfe956f4e21ff54bd73a
tarunnjoshi <tarun12joshi@gmail.com>
2025-04-15 15:08:25 +0530

    add quer params

--- wp-content/civi-extensions/goonjcustom/Civi/GenerateMaterialReceiptService.php
+++ wp-content/civi-extensions/goonjcustom/Civi/GenerateMaterialReceiptService.php
@@ -45,12 +45,21 @@ class GenerateMaterialReceiptService extends AutoSubscriber {
     error_log("activityId: " . print_r($activityId, TRUE));
 
     $activities = Activity::get(TRUE)
-      ->addSelect('Material_Contribution.Collection_Camp', 'Institution_Material_Contribution.Description_of_Material_No_of_Bags_Material_', 'subject', 'Material_Contribution.Delivered_By', 'id')
+      ->addSelect('*', 'contact.display_name', 'Material_Contribution.Delivered_By', 'Material_Contribution.Delivered_By_Contact', 'Material_Contribution.Goonj_Office', 'Material_Contribution.Collection_Camp.subtype:name', 'Material_Contribution.Institution_Collection_Camp.subtype:name', 'Material_Contribution.Dropping_Center.subtype:name', 'Material_Contribution.Institution_Dropping_Center.subtype:name', 'Material_Contribution.Contribution_Date')
       ->addWhere('Material_Contribution.Collection_Camp', '=', $campId)
       ->addWhere('id', '=', $activityId)
-      ->execute()->first();
+      ->execute();
 
-    $entityId = $activities['id'];
+    $contribution = $activities->first();
+    $descriptionOfMaterial = $contribution['subject'] ?? NULL;
+    $contributionDate = $contribution['Material_Contribution.Contribution_Date'];
+    $activityDate = date("F j, Y", strtotime($activity['activity_date_time']));
+    // $receivedOnDate = !empty($contributionDate)
+    // ? date("F j, Y", strtotime($contributionDate))
+    // : $activityDate;
+
+
+    $entityId = $contribution['id'];
     error_log("entityId: " . print_r($entityId, TRUE));
 
     self::generatePdfForCollectionCamp($entityId);
