847495f01c53eae0a20140f246d614c115fa09c2
tarunnjoshi <tarun12joshi@gmail.com>
2025-04-17 15:39:26 +0530

    remove logs

--- wp-content/civi-extensions/goonjcustom/Civi/GenerateMaterialReceiptService.php
+++ wp-content/civi-extensions/goonjcustom/Civi/GenerateMaterialReceiptService.php
@@ -33,18 +33,15 @@ class GenerateMaterialReceiptService extends AutoSubscriber {
    */
   public static function generateMaterialReceipt(string $op, string $objectName, int $objectId, &$objectRef) {
     if ($op !== 'create' || $objectName !== 'AfformSubmission' || empty($objectRef->afform_name) || $objectRef->afform_name !== 'afformSendReminderToCollectionCampMaterialContributions') {
-      error_log("Conditions not met for generating PDF.");
       return;
     }
+
     $data = json_decode($objectRef->data, TRUE);
 
     $activityData = $data['Eck_Collection_Camp1'][0]['fields'] ?? [];
 
     $campId = $data['Eck_Collection_Camp1'][0]['fields']['id'] ?? NULL;
     $activityId = $data['Activity1'][0]['fields']['id'] ?? NULL;
-
-    error_log("campId: " . print_r($campId, TRUE));
-    error_log("activityId: " . print_r($activityId, TRUE));
 
     $activities = Activity::get(TRUE)
       ->addSelect('*', 'contact.display_name', 'Material_Contribution.Delivered_By', 'Material_Contribution.Delivered_By_Contact', 'Material_Contribution.Goonj_Office', 'Material_Contribution.Collection_Camp.subtype:name', 'Material_Contribution.Institution_Collection_Camp.subtype:name', 'Material_Contribution.Dropping_Center.subtype:name', 'Material_Contribution.Institution_Dropping_Center.subtype:name', 'Material_Contribution.Contribution_Date')
@@ -88,7 +85,6 @@ class GenerateMaterialReceiptService extends AutoSubscriber {
     $deliveredByContact = empty($contribution['Material_Contribution.Delivered_By_Contact']) ? $phone : $contribution['Material_Contribution.Delivered_By_Contact'];
 
     $entityId = $contribution['id'];
-    error_log("entityId: " . print_r($entityId, TRUE));
 
     // self::generatePdfForCollectionCamp($entityId, $descriptionOfMaterial, $receivedOnDate, $contributionVenue, $email, $phone, $deliveredBy, $deliveredByContact );.
     QrCodeable::generatePdfForCollectionCamp($entityId, $contribution, $email, $phone, $contributionVenue, $contributionDate);
