fab9a56cb28f2c0ba9f473f3cca91f4b47a9c8cb
tarunnjoshi <tarun12joshi@gmail.com>
2025-04-18 11:51:44 +0530

    dwonload pdf for receipt

--- wp-content/civi-extensions/goonjcustom/Civi/GenerateMaterialReceiptService.php
+++ wp-content/civi-extensions/goonjcustom/Civi/GenerateMaterialReceiptService.php
@@ -7,6 +7,7 @@ require_once __DIR__ . '/../../../../wp-content/civi-extensions/goonjcustom/vend
 use Civi\Api4\Activity;
 use Civi\Api4\EckEntity;
 use Civi\Api4\Contact;
+use Civi\Api4\Organization;
 
 use Civi\Core\Service\AutoSubscriber;
 use Civi\Traits\QrCodeable;
@@ -33,43 +34,63 @@ class GenerateMaterialReceiptService extends AutoSubscriber {
    *
    */
   public static function generateMaterialReceipt(string $op, string $objectName, int $objectId, &$objectRef) {
-    if ($op !== 'create' || $objectName !== 'AfformSubmission' || empty($objectRef->afform_name) || $objectRef->afform_name !== 'afformSendReminderToCollectionCampMaterialContributions') {
+    if (
+      $op !== 'create' ||
+      $objectName !== 'AfformSubmission' ||
+      empty($objectRef->afform_name) ||
+      (
+          $objectRef->afform_name !== 'afformSendReminderToCollectionCampMaterialContributions' &&
+          $objectRef->afform_name !== 'afformSendReminderToGoonjOffice'
+      )
+    ) {
       return;
     }
 
     $data = json_decode($objectRef->data, TRUE);
 
     $campId = $data['Eck_Collection_Camp1'][0]['fields']['id'] ?? NULL;
+    $puId = $data['Activity1'][0]['fields']['Material_Contribution.Goonj_Office'] ?? NULL;
     $activityId = $data['Activity1'][0]['fields']['id'] ?? NULL;
 
-    $collectionCamp = EckEntity::get('Collection_Camp', FALSE)
-      ->addSelect('subtype:name')
-      ->addWhere('id', '=', $campId)
-      ->execute()->first();
+    if ($campId) {
 
-    $subtype = $collectionCamp['subtype:name'];
+      $collectionCamp = EckEntity::get('Collection_Camp', FALSE)
+        ->addSelect('subtype:name')
+        ->addWhere('id', '=', $campId)
+        ->execute()->first();
 
-    if ($subtype == 'Collection_Camp') {
-      $contributionName = 'Material_Contribution.Collection_Camp';
-    }
-    elseif ($subtype == 'Dropping_Center') {
-      $contributionName = 'Material_Contribution.Dropping_Center';
-    }
-    elseif ($subtype == 'Institution_Collection_Camp') {
-      $contributionName = 'Material_Contribution.Institution_Collection_Camp';
-    }
-    elseif ($subtype == 'Institution_Dropping_Center') {
-      $contributionName = 'Material_Contribution.Institution_Dropping_Center';
-    }
+      $subtype = $collectionCamp['subtype:name'];
 
+      if ($subtype == 'Collection_Camp') {
+        $contributionName = 'Material_Contribution.Collection_Camp';
+      }
+      elseif ($subtype == 'Dropping_Center') {
+        $contributionName = 'Material_Contribution.Dropping_Center';
+      }
+      elseif ($subtype == 'Institution_Collection_Camp') {
+        $contributionName = 'Material_Contribution.Institution_Collection_Camp';
+      }
+      elseif ($subtype == 'Institution_Dropping_Center') {
+        $contributionName = 'Material_Contribution.Institution_Dropping_Center';
+      }
 
-    $activities = Activity::get(TRUE)
-      ->addSelect('*', 'contact.display_name', 'Material_Contribution.Delivered_By', 'Material_Contribution.Delivered_By_Contact', 'Material_Contribution.Goonj_Office', 'Material_Contribution.Collection_Camp.subtype:name', 'Material_Contribution.Institution_Collection_Camp.subtype:name', 'Material_Contribution.Dropping_Center.subtype:name', 'Material_Contribution.Institution_Dropping_Center.subtype:name', 'Material_Contribution.Contribution_Date', 'source_contact_id', 'activity_date_time', 'subject')
-      ->addWhere($contributionName, '=', $campId)
-      ->addWhere('id', '=', $activityId)
-      ->addJoin('ActivityContact AS activity_contact', 'LEFT')
-      ->addJoin('Contact AS contact', 'LEFT')
-      ->execute();
+      $activities = Activity::get(TRUE)
+        ->addSelect('*', 'contact.display_name', 'Material_Contribution.Delivered_By', 'Material_Contribution.Delivered_By_Contact', 'Material_Contribution.Goonj_Office', 'Material_Contribution.Collection_Camp.subtype:name', 'Material_Contribution.Institution_Collection_Camp.subtype:name', 'Material_Contribution.Dropping_Center.subtype:name', 'Material_Contribution.Institution_Dropping_Center.subtype:name', 'Material_Contribution.Contribution_Date', 'source_contact_id', 'activity_date_time', 'subject')
+        ->addWhere($contributionName, '=', $campId)
+        ->addWhere('id', '=', $activityId)
+        ->addJoin('ActivityContact AS activity_contact', 'LEFT')
+        ->addJoin('Contact AS contact', 'LEFT')
+        ->execute();
+    }
+    else {
+      $activities = Activity::get(TRUE)
+        ->addSelect('*', 'contact.display_name', 'Material_Contribution.Delivered_By', 'Material_Contribution.Delivered_By_Contact', 'Material_Contribution.Goonj_Office', 'Material_Contribution.Collection_Camp.subtype:name', 'Material_Contribution.Institution_Collection_Camp.subtype:name', 'Material_Contribution.Dropping_Center.subtype:name', 'Material_Contribution.Institution_Dropping_Center.subtype:name', 'Material_Contribution.Contribution_Date', 'source_contact_id', 'activity_date_time', 'subject')
+        ->addWhere('Material_Contribution.Goonj_Office', '=', $puId)
+        ->addWhere('id', '=', $activityId)
+        ->addJoin('ActivityContact AS activity_contact', 'LEFT')
+        ->addJoin('Contact AS contact', 'LEFT')
+        ->execute();
+    }
 
     $contribution = $activities->first();
 
@@ -103,7 +124,6 @@ class GenerateMaterialReceiptService extends AutoSubscriber {
 
     $entityId = $contribution['id'];
 
-    // self::generatePdfForCollectionCamp($entityId, $descriptionOfMaterial, $receivedOnDate, $contributionVenue, $email, $phone, $deliveredBy, $deliveredByContact );.
     QrCodeable::generatePdfForCollectionCamp($entityId, $contribution, $email, $phone, $contributionVenue, $contributionDate);
 
   }
