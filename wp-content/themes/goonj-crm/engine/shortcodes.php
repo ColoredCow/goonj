ef035e15e9149d62336b1e907bd10f8428eba4e1
belwalshubham <belwalshubham08@gmail.com>
2025-05-07 10:30:20 +0530

    feat: add monetary contribution URL with prefilled source

--- wp-content/themes/goonj-crm/engine/shortcodes.php
+++ wp-content/themes/goonj-crm/engine/shortcodes.php
@@ -33,56 +33,104 @@ function goonj_contribution_volunteer_signup_button() {
 	if ( empty( $activity_id ) ) {
 		\Civi::log()->warning( 'Activity ID is missing' );
 		return;
-	}
+    }
 
-	try {
+    try {
 		$activities = \Civi\Api4\Activity::get( false )
 			->addSelect( 'source_contact_id' )
 			->addJoin( 'ActivityContact AS activity_contact', 'LEFT' )
 			->addWhere( 'id', '=', $activity_id )
 			->addWhere( 'activity_type_id:label', '=', 'Material Contribution' )
-			->execute();
+            ->execute();
 
 		if ( $activities->count() === 0 ) {
 			\Civi::log()->info( 'No activities found for Activity ID:', array( 'activityId' => $activity_id ) );
 			return;
-		}
+        }
 
-		$activity = $activities->first();
-		$individual_id = $activity['source_contact_id'];
+        $activity = $activities->first();
+        $individual_id = $activity['source_contact_id'];
 
 		$contact = \Civi\Api4\Contact::get( false )
 			->addSelect( 'contact_sub_type' )
 			->addWhere( 'id', '=', $individual_id )
-			->execute()
-			->first();
+            ->execute()
+            ->first();
 
 		if ( empty( $contact ) ) {
 			\Civi::log()->info( 'Contact not found', array( 'contact' => $contact['id'] ) );
 			return;
-		}
+        }
 
 		$contactSubTypes = $contact['contact_sub_type'] ?? array();
+        $buttonsHtml = '';
 
-		// If the individual is already a volunteer, don't show the button
-		if ( in_array( 'Volunteer', $contactSubTypes ) ) {
-			return;
-		}
+        // Generate volunteer signup button if not already a volunteer
+        if (!in_array('Volunteer', $contactSubTypes)) {
+            $redirectPath = '/volunteer-registration/form-with-details/';
+            $redirectPathWithParams = $redirectPath . '#?' . http_build_query([
+                'Individual1' => $individual_id,
+                'message' => 'individual-user',
+            ]);
+            $buttonText = __('Wish to Volunteer?', 'goonj-crm');
+            $buttonsHtml .= goonj_generate_button_html($redirectPathWithParams, $buttonText);
+        }
 
-		$redirectPath = '/volunteer-registration/form-with-details/';
-		$redirectPathWithParams = $redirectPath . '#?' . http_build_query(
-			array(
-				'Individual1' => $individual_id,
-				'message' => 'individual-user',
-			)
-		);
-		$buttonText = __( 'Wish to Volunteer?', 'goonj-crm' );
+        $activityDetails = \Civi\Api4\Activity::get(false)
+            ->addSelect(
+                'Material_Contribution.Collection_Camp',
+                'Material_Contribution.Dropping_Center',
+                'Material_Contribution.Institution_Collection_Camp',
+                'Material_Contribution.Institution_Dropping_Center'
+            )
+            ->addWhere('id', '=', $activity_id)
+            ->execute()
+            ->first();
 
-		return goonj_generate_button_html( $redirectPathWithParams, $buttonText );
-	} catch ( \Exception $e ) {
-		\Civi::log()->error( 'Error in goonj_contribution_volunteer_signup_button: ' . $e->getMessage() );
-		return;
-	}
+        $fields = [
+            'Material_Contribution.Collection_Camp',
+            'Material_Contribution.Dropping_Center',
+            'Material_Contribution.Institution_Collection_Camp',
+            'Material_Contribution.Institution_Dropping_Center',
+        ];
+
+        $selectedValue = null;
+        foreach ($fields as $field) {
+            if (!empty($activityDetails[$field])) {
+                $selectedValue = $activityDetails[$field];
+                break;
+            }
+        }
+
+        // Generate monetary contribution button if a value is found
+        if ($selectedValue) {
+            // Get custom field ID for "Source"
+            $sourceField = \Civi\Api4\CustomField::get(false)
+                ->addSelect('id')
+                ->addWhere('custom_group_id:name', '=', 'Contribution_Details')
+                ->addWhere('name', '=', 'Source')
+                ->execute()
+                ->single();
+
+            $sourceFieldId = 'custom_' . $sourceField['id'];
+
+            // Generate checksum for secure URL
+            $checksum = \Civi\Api4\Contact::getChecksum(false)
+                ->setContactId($individual_id)
+                ->setTtl(7 * 24 * 60 * 60)
+                ->execute()
+                ->first()['checksum'];
+
+            $monetaryUrl = "/contribute/?$sourceFieldId=$selectedValue&cid=$individual_id&cs=$checksum";
+            $monetaryButtonText = __('Monetary Contribution', 'goonj-crm');
+            $buttonsHtml .= goonj_generate_button_html($monetaryUrl, $monetaryButtonText);
+        }
+
+        return $buttonsHtml;
+    } catch (\Exception $e) {
+        \Civi::log()->error('Error in goonj_contribution_volunteer_signup_button: ' . $e->getMessage());
+        return '';
+    }
 }
 
 function goonj_pu_activity_button() {
@@ -134,57 +182,49 @@ function goonj_collection_camp_past_data() {
 }
 
 function goonj_induction_slot_details() {
-    // Retrieve parameters from the GET request
     $source_contact_id = intval($_GET['source_contact_id'] ?? 0);
     $slot_date = $_GET['slot_date'] ?? '';
     $slot_time = $_GET['slot_time'] ?? '';
     $inductionType = $_GET['induction_type'] ?? '';
 
     try {
-        // Fetch the induction activity for the specified source contact
         $inductionActivity = \Civi\Api4\Activity::get(FALSE)
             ->addSelect('id', 'activity_date_time', 'status_id:name', 'Induction_Fields.Goonj_Office', 'Induction_Fields.Assign')
             ->addWhere('source_contact_id', '=', $source_contact_id)
             ->addWhere('activity_type_id:name', '=', 'Induction')
             ->execute()->single();
 
-        // Exit if no activity found or the status is not "To be Scheduled"
         if (!$inductionActivity || $inductionActivity['status_id:name'] !== 'To be scheduled') {
             \Civi::log()->info('No valid activity found or status is not To be Scheduled', ['contact_id' => $source_contact_id]);
             return;
         }
 
-		// Combine slot date (d-m-Y) and slot time (h:i A) to form new activity date and time
 		$newActivityDateTime = DateTime::createFromFormat('d-m-Y h:i A', "$slot_date $slot_time");
 		if (!$newActivityDateTime) {
 			\Civi::log()->error('Invalid date/time format', ['slot_date' => $slot_date, 'slot_time' => $slot_time]);
 			return;
 		}
 
-
-        // Update activity date and time and set the status to "Scheduled"
         \Civi\Api4\Activity::update(FALSE)
             ->addValue('activity_date_time', $newActivityDateTime->format('Y-m-d H:i:s'))
             ->addValue('status_id:name', 'Scheduled')
             ->addValue('Induction_Fields.Mode:name', $inductionType)
             ->addWhere('id', '=', $inductionActivity['id'])
             ->execute();
+
         \Civi::log()->info('Activity updated successfully', [
             'activity_id' => $inductionActivity['id'],
             'new_date_time' => $newActivityDateTime->format('Y-m-d H:i:s')
         ]);
 
-        // Select the email template based on the induction type
         $templateTitle = ($inductionType == 'Processing_Unit') 
             ? 'Acknowledgment_for_Induction_Slot_Booked' 
             : 'Acknowledgment_for_Online_Induction_Slot_Booked';
 
-        // Fetch the email template
         $template = \Civi\Api4\MessageTemplate::get(FALSE)
             ->addWhere('msg_title', 'LIKE', $templateTitle . '%')
             ->execute()->single();
 
-        // If a template is found, send the email notification
         if ($template) {
             $assigneeContact = \Civi\Api4\Contact::get(FALSE)
                 ->addSelect('email.email')
@@ -192,14 +232,12 @@ function goonj_induction_slot_details() {
                 ->addWhere('id', '=', $inductionActivity['Induction_Fields.Assign'])
 				->execute()->single();
 
-            // Prepare email parameters
             $emailParams = [
                 'contact_id' => $source_contact_id,
                 'template_id' => $template['id'],
                 'cc' => $assigneeContact['email.email']
             ];
 
-            // Send the email
             $emailResult = civicrm_api3('Email', 'send', $emailParams);
         }
     } catch (\Exception $e) {
