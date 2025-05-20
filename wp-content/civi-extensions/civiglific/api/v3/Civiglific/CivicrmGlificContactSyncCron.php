333207f666f4243a066453d249d80b475c840465
tarunnjoshi <tarun12joshi@gmail.com>
2025-05-20 10:38:41 +0530

    add logs and functionn for opt in

--- wp-content/civi-extensions/civiglific/api/v3/Civiglific/CivicrmGlificContactSyncCron.php
+++ wp-content/civi-extensions/civiglific/api/v3/Civiglific/CivicrmGlificContactSyncCron.php
@@ -54,13 +54,19 @@ function civicrm_api3_civiglific_civicrm_glific_contact_sync_cron($params) {
     $glificPhones = _getGlificContactsFromGroup($glificGroupId);
 
     $civiPhones = array_column($civiContacts, 'phone');
+  error_log('civiPhones: ' . print_r($civiPhones, TRUE));
+
 
     // 4. Add new contacts to Glific group.
     foreach ($civiContacts as $contact) {
       try {
         if (!in_array($contact['phone'], $glificPhones)) {
           $glificId = _createGlificContact($contact['name'], $contact['phone']);
+  error_log('glificId: ' . print_r($glificId, TRUE));
+
           if ($glificId) {
+            // Opt-in contact after creation
+            // _optinGlificContact($contact['phone'], $contact['name']);
             _addContactToGlificGroup($glificId, $glificGroupId);
           }
           else {
@@ -284,3 +290,40 @@ function _glificGraphQLQuery($query, $variables = []) {
 function _normalizePhone($phone) {
   return preg_replace('/\D+/', '', $phone);
 }
+
+// /**
+//  * Opt-in a contact in Glific.
+//  */
+// function _optinGlificContact($phone, $name = NULL) {
+//   $query = '
+//     mutation optinContact($phone: String!, $name: String) {
+//       optinContact(phone: $phone, name: $name) {
+//         contact {
+//           id
+//           phone
+//           name
+//           optinTime
+//         }
+//         errors {
+//           key
+//           message
+//         }
+//       }
+//     }
+//   ';
+
+//   $variables = [
+//     'phone' => $phone,
+//     'name' => $name,
+//   ];
+
+//   $response = _glificGraphQLQuery($query, $variables);
+//   error_log('respose: ' . print_r($response, TRUE));
+
+//   // Optional: log if opt-in failed.
+//   if (!empty($response['data']['optinContact']['errors'])) {
+//     \Civi::log()->error("Glific opt-in error for {$phone}: " . json_encode($response['data']['optinContact']['errors']));
+//   }
+
+//   return $response['data']['optinContact']['contact']['id'] ?? NULL;
+// }
