d05f1190c27d895566032fcd568cfadab7bf67ab
belwalshubham <belwalshubham08@gmail.com>
2025-06-04 19:05:37 +0530

    code cleanups

--- wp-content/civi-extensions/goonjcustom/api/v3/ContributionFilter.php
+++ wp-content/civi-extensions/goonjcustom/api/v3/ContributionFilter.php
@@ -2,7 +2,6 @@
 
 require_once 'api/Wrapper.php';
 
-use Civi\API\Exception\UnauthorizedException;
 use Civi\Api4\Campaign;
 use Civi\Api4\Generic\AbstractAction;
 
@@ -11,19 +10,12 @@ use Civi\Api4\Generic\AbstractAction;
  */
 class CRM_Goonjcustom_APIWrappers_ContributionFilter implements API_Wrapper {
 
+  private $allowAccess = TRUE;
+
   /**
-   * Process input parameters before the API call.
-   * Throws an exception if the campaign’s access flag is not set to allow access.
    *
-   * @param \Civi\Api4\Generic\AbstractAction $apiRequest
-   *   The API request object.
-   *
-   * @return \Civi\Api4\Generic\AbstractAction The modified API request object
-   *
-   * @throws \Civi\Api4\Exception\APIException If access is denied or campaign is not found
    */
   public function fromApiInput($apiRequest) {
-    // Validate that we have an AbstractAction object.
     if (!($apiRequest instanceof AbstractAction)) {
       throw new \InvalidArgumentException('Expected Civi\Api4\Generic\AbstractAction');
     }
@@ -46,32 +38,29 @@ class CRM_Goonjcustom_APIWrappers_ContributionFilter implements API_Wrapper {
         ->addWhere('id', '=', $id)
         ->execute()->first();
 
-      if (!empty($campaigns) && isset($campaigns['Api_Access_Control.Allow_Api_Access'])) {
-        $flag = $campaigns['Api_Access_Control.Allow_Api_Access'];
-
-        if ($flag != 1) {
-          throw new UnauthorizedException('You don’t have access to this campaign.');
-        }
+      if (empty($campaigns) || $campaigns['Api_Access_Control.Allow_Api_Access'] != 1) {
+        $this->allowAccess = FALSE;
       }
-      else {
-        throw new UnauthorizedException('Campaign not found or access flag not set.');
-      }
+    }
+    else {
+      $this->allowAccess = FALSE;
     }
 
     return $apiRequest;
   }
 
   /**
-   * Pass the API output through unmodified.
    *
-   * @param \Civi\Api4\Generic\AbstractAction $apiRequest
-   *   The API request object.
-   * @param mixed $result
-   *   The API result.
-   *
-   * @return mixed The unmodified result
    */
   public function toApiOutput($apiRequest, $result) {
+    if (!$this->allowAccess) {
+      $message = 'You do not have permission to view the details for this campaign ID.';
+      foreach ($result as &$row) {
+        unset($row['id']);
+        $row['Contribution_Data.Total_Contribution_Amount'] = $message;
+        $row['Contribution_Data.Total_Number_of_Contributors'] = $message;
+      }
+    }
     return $result;
   }
 
