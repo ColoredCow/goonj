60bc8c462b37b6f3825ea1593a1148eb5be746ae
tarunnjoshi <tarun12joshi@gmail.com>
2025-06-23 10:16:50 +0530

    set css for first div

--- wp-content/themes/goonj-crm/goonj-contribution.js
+++ wp-content/themes/goonj-crm/goonj-contribution.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var donateMonthlyRadio = document.querySelector('.crm-contribution-main-form-block .custom_pre_profile-group fieldset .crm-section .content .crm-multiple-checkbox-radio-options .crm-option-label-pair input.crm-form-radio[value="2"]');
     var recurringSection = document.querySelector('.crm-public-form-item.crm-section.is_recur-section');
     var fieldset = document.querySelector('.crm-public-form-item.crm-group.custom_pre_profile-group fieldset');
-    var firstDiv = document.getElementById('editrow-custom_759'); // Use ID for precision
+    var firstDiv = document.querySelector('.crm-contribution-main-form-block .custom_pre_profile-group fieldset div:first-of-type');
     var radioButtons = document.querySelectorAll('.crm-contribution-main-form-block .custom_pre_profile-group fieldset .crm-section .content .crm-multiple-checkbox-radio-options .crm-option-label-pair input.crm-form-radio');
 
     // Function to apply styles to recurring section and first div
