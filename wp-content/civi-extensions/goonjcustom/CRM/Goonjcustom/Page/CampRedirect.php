596f87d68fe7fbcfb0e295a5fe5401f52116c204
tarunnjoshi <tarun12joshi@gmail.com>
2025-09-05 13:05:01 +0530

    add try catch

--- wp-content/civi-extensions/goonjcustom/CRM/Goonjcustom/Page/CampRedirect.php
+++ wp-content/civi-extensions/goonjcustom/CRM/Goonjcustom/Page/CampRedirect.php
@@ -16,8 +16,14 @@ class CRM_Goonjcustom_Page_CampRedirect extends CRM_Core_Page {
    */
   public function run() {
     $id = CRM_Utils_Request::retrieve('id', 'Positive', $this, TRUE);
-    QrCodeable::handleCampRedirect($id);
-    CRM_Utils_System::civiExit();
+    try {
+      QrCodeable::handleCampRedirect($id);
+      CRM_Utils_System::civiExit();
+    }
+    catch (\Throwable $e) {
+      \Civi::log()->error("CampRedirect failed for id={$id}: " . $e->getMessage());
+      CRM_Core_Error::statusBounce(ts('This link is invalid or has expired.'));
+    }
   }
 
 }
