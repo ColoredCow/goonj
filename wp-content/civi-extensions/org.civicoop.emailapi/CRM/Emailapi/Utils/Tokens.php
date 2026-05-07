95eaef0fa663f4e637cfc41cecf62f00c834466b
tarunnjoshi <tarun12joshi@gmail.com>
2026-05-07 14:45:29 +0530

    Email API (2.22.0 → 2.26.1)

--- wp-content/civi-extensions/org.civicoop.emailapi/CRM/Emailapi/Utils/Tokens.php
+++ wp-content/civi-extensions/org.civicoop.emailapi/CRM/Emailapi/Utils/Tokens.php
@@ -27,10 +27,11 @@ class CRM_Emailapi_Utils_Tokens {
    * @param int $contactId
    * @param array $message
    * @param array $contactData
+   * @param string $preferred_language
    *
    * @return string[]
    */
-  public static function replaceTokens(int $contactId, array $message, array $contactData=[]): array {
+  public static function replaceTokens(int $contactId, array $message, array $contactData=[], string $preferred_language = NULL): array {
     // Add the entities we want rendered into the schema, and record their primary keys.
     $schema['contactId'] = 'contactId';
     $context['contactId'] = $contactId;
@@ -64,101 +65,22 @@ class CRM_Emailapi_Utils_Tokens {
     $tokenProcessor->addMessage('html', $message['html'], 'text/html');
     $tokenProcessor->addMessage('text', $message['text'], 'text/plain');
     $row = $tokenProcessor->addRow($context);
+
+    // Set language on row if necessary
+    $swapLocale = NULL;
+    if ($preferred_language) {
+      $row->context('locale', $preferred_language);
+      $swapLocale = \CRM_Utils_AutoClean::swapLocale($preferred_language);
+    }
+
     // Evaluate and render.
     $tokenProcessor->evaluate();
     foreach (['messageSubject', 'html', 'text'] as $component) {
       $rendered[$component] = $row->render($component);
     }
 
+    unset($swapLocale);
     return $rendered;
-  }
-
-  /**
-   * Get the categories required for rendering tokens.
-   *
-   * @return array
-   */
-  protected static function getTokenCategories() {
-    if (!isset(\Civi::$statics[__CLASS__]['token_categories'])) {
-      $tokens = [];
-      \CRM_Utils_Hook::tokens($tokens);
-      \Civi::$statics[__CLASS__]['token_categories'] = array_keys($tokens);
-    }
-    return \Civi::$statics[__CLASS__]['token_categories'];
-  }
-
-  /**
-   * @param $contact_id
-   * @param $returnProperties
-   *
-   * @return mixed
-   * @throws \CRM_Core_Exception
-   */
-  protected static function getTokenDetails($contact_id, $returnProperties=NULL) {
-    $params = [];
-    $params[] = [
-      \CRM_Core_Form::CB_PREFIX . $contact_id,
-      '=',
-      1,
-      0,
-      0,
-    ];
-    if (empty($returnProperties)) {
-      $fields = array_merge(array_keys(\CRM_Contact_BAO_Contact::exportableFields()),
-        ['display_name', 'checksum', 'contact_id']
-      );
-      foreach ($fields as $val) {
-        // The unavailable fields are not available as tokens, do not have a one-2-one relationship
-        // with contacts and are expensive to resolve.
-        // @todo see CRM-17253 - there are some other fields (e.g note) that should be excluded
-        // and upstream calls to this should populate return properties.
-        $unavailableFields = ['group', 'tag'];
-        if (!in_array($val, $unavailableFields)) {
-          $returnProperties[$val] = 1;
-        }
-      }
-    }
-
-    $custom = [];
-    foreach ($returnProperties as $name => $dontCare) {
-      $cfID = \CRM_Core_BAO_CustomField::getKeyID($name);
-      if ($cfID) {
-        $custom[] = $cfID;
-      }
-    }
-
-    $details = \CRM_Contact_BAO_Query::apiQuery($params, $returnProperties, NULL, NULL, 0, 1, TRUE, FALSE, TRUE, \CRM_Contact_BAO_Query::MODE_CONTACTS, NULL, TRUE);
-    $contactDetails = &$details[0];
-    if (array_key_exists($contact_id, $contactDetails)) {
-      if (!empty($contactDetails[$contact_id]['preferred_communication_method'])
-      ) {
-        $communicationPreferences = [];
-        foreach ($contactDetails[$contact_id]['preferred_communication_method'] as $val) {
-          if ($val) {
-            $communicationPreferences[$val] = \CRM_Core_PseudoConstant::getLabel('CRM_Contact_DAO_Contact', 'preferred_communication_method', $val);
-          }
-        }
-        $contactDetails[$contact_id]['preferred_communication_method'] = implode(', ', $communicationPreferences);
-      }
-
-      foreach ($custom as $cfID) {
-        if (isset($contactDetails[$contact_id]["custom_{$cfID}"])) {
-          $contactDetails[$contact_id]["custom_{$cfID}"] = \CRM_Core_BAO_CustomField::displayValue($contactDetails[$contact_id]["custom_{$cfID}"], $cfID);
-        }
-      }
-
-      // special case for greeting replacement
-      foreach ([
-                 'email_greeting',
-                 'postal_greeting',
-                 'addressee',
-               ] as $val) {
-        if (!empty($contactDetails[$contact_id][$val])) {
-          $contactDetails[$contact_id][$val] = $contactDetails[$contact_id]["{$val}_display"];
-        }
-      }
-    }
-    return $contactDetails[$contact_id];
   }
 
 }
